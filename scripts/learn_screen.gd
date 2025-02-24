e4cdcde1f75e5585b4be1d1445894b79b99253c1
LiamLucRice <liam@eoi-b4purpose.com>
2025-02-24 16:59:00 +0700

    Set up basic data structures

--- scripts/learn_screen.gd
+++ scripts/learn_screen.gd
@@ -10,11 +10,22 @@ signal _back_button_pressed
 @onready var learn_words_button = $LearningPanel/VBoxContainer/VBoxContainer/LearnWordsButton
 @onready var back_button = $LearningPanel/VBoxContainer/BackButton
 
-# Panels
+# Main Panel
 @onready var main_panel = $LearningPanel
 @onready var exercise_type_1 = $LearningPanelExerciseType1
 @onready var exercise_type_2 = $LearningPanelExerciseType2
 @onready var exercise_type_3 = $LearningPanelExerciseType3
+# Exercise Panels
+@onready var exercise_panel_1 = $LearningPanelExerciseType1
+@onready var exercise_label = $LearningPanelExerciseType1/VBoxContainer/VBoxContainer/ExerciseLabel
+@onready var exercise_buttons = [
+	$LearningPanelExerciseType1/VBoxContainer/VBoxContainer/HBoxContainer/GridContainer/OptionButton1,
+	$LearningPanelExerciseType1/VBoxContainer/VBoxContainer/HBoxContainer/GridContainer/OptionButton2,
+	$LearningPanelExerciseType1/VBoxContainer/VBoxContainer/HBoxContainer/GridContainer/OptionButton3,
+	$LearningPanelExerciseType1/VBoxContainer/VBoxContainer/HBoxContainer/GridContainer/OptionButton4
+]
+@onready var check_continue_button = $LearningPanelExerciseType1/VBoxContainer/VBoxContainer/CheckContinueButton
+@onready var quict_lesson_button = $LearningPanelExerciseType1/VBoxContainer/QuitLessonButton
 
 # stages
 enum Stages {
@@ -27,7 +38,6 @@ var current_panel:Panel
 
 # set management
 var letter_set:Array[int]
-var letter_confusion:Array[int]
 
 
 func _ready():
@@ -38,6 +48,11 @@ func _ready():
 
 func generate_letters_set():
 	# fetch the indices of the letters for this set
+	pass
+
+
+func generate_next_exercise_letters_type_1():
+	# generate the question and 4 options, including which one is the correct option
 	pass
 
 
@@ -53,6 +68,7 @@ func _on_back_button_pressed():
 func _on_learn_letters_button_pressed():
 	count = 0
 	stage = Stages.LEARNING_LETTERS
+	generate_letters_set()
 	next_exercise()
 
 
@@ -60,6 +76,7 @@ func next_exercise():
 	var rand = RandomNumberGenerator.new()
 	var i = rand.randi_range(1, 3)
 	if i==1:
+		generate_next_exercise_letters_type_1()
 		switch_screen(current_panel, exercise_type_1)
 		current_panel = exercise_type_1
 	elif i==2:
